4f67a5f428722e62dd03cf99c6d3968439922f83
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-15 00:34:52 +0530

    Added click event for item

--- renderer.js
+++ renderer.js
@@ -10,12 +10,20 @@ const App = new Vue({
     mounted() {
         setInterval(this.checkClipboard, 500)
     },
+    computed: {
+        reversedHistory() {
+            return this.history.slice().reverse()
+        }
+    },
     methods: {
         checkClipboard() {
             const text = clipboard.readText()
             if (this.history[this.history.length - 1] !== text) {
                 this.history.push(text)
             }
+        },
+        itemClicked(item) {
+            clipboard.writeText(item);
         }
     }
 })
